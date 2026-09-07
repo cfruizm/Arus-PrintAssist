a76489423a7162207c1dd99c26f805d507e52f4b
Cristian Ruiz <99301062+cfruizm@users.noreply.github.com>
2026-09-07 08:52:24 -0500

    Delete app/agent_core_v2/interpreter_diagnostics.py

--- app/agent_core_v2/interpreter_diagnostics.py
+++
@@ -1,4 +0,0 @@
-from app.agent_core_v2.interpreter import ignored_interpreter_fields
-
-def summarize_ignored_fields(raw_payload):
-    return {"ignored_fields": ignored_interpreter_fields(raw_payload)}
